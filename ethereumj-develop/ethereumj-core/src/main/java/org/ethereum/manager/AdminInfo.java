aaf781638ed46293f6be3d37c50b495c4ed165da
phenix <phenix@fancy.com>
2020-11-11 14:26:30 +0800

    update

--- ethereumj-develop/ethereumj-core/src/main/java/org/ethereum/manager/AdminInfo.java
+++ ethereumj-develop/ethereumj-core/src/main/java/org/ethereum/manager/AdminInfo.java
@@ -28,15 +28,30 @@ import java.util.List;
  * @author Roman Mandeleil
  * @since 11.12.2014
  */
+
+
+//1、@Service用于标注业务层组件
+//2、@Controller用于标注控制层组件(如struts中的action)
+//3、@Repository用于标注数据访问组件，即DAO组件.
+// 4、@Component泛指组件，当组件不好归类的时候，我们可以使用这个注解进行标注。
 @Component
 public class AdminInfo {
     private static final int ExecTimeListLimit = 10000;
 
     private long startupTimeStamp;
+    //共识
     private boolean consensus = true;
     private List<Long> blockExecTime = new LinkedList<>();
 
 
+
+//    @PostConstruct该注解被用来修饰一个非静态的void（）方法。被@PostConstruct修饰的方法会在服务器加载Servlet的时候运行，并且只会被服务器执行一次。PostConstruct在构造函数之后执行，init（）方法之前执行。
+//
+//    通常我们会是在Spring框架中使用到@PostConstruct注解 该注解的方法在整个Bean初始化中的执行顺序：
+//
+//    Constructor(构造方法) -> @Autowired(依赖注入) -> @PostConstruct(注释的方法)
+//
+//    应用：在静态方法中调用依赖注入的Bean中的方法。
     @PostConstruct
     public void init() {
         startupTimeStamp = System.currentTimeMillis();
